7b19847b6af9971e94d334226a22afeb723bec2a
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 14:54:17 +0000

    mflksam

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  validates_presence_of :name      
+  validates_presence_of :name    
+  
   def first_name
     self.name.split.first
   end
   
   def last_name
     self.name.split.last
+  end
+end
+
+def get_facebook_messages
+  begin
+    contacts_fb
+    @messages = retrieves_messages
+  rescue IOError => e
+    flash[:error] = "Error occurred contacting Facebook #{e}"
   end
 end
