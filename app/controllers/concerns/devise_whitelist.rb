bbc20cb2e62986eb89f05859985c73f80fda3280
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 15:35:56 +0000

    Trying to make this pos work

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,8 +1,5 @@
 module DeviseWhitelist
    extend ActiveSupport::Concern
-
-
-  extend ActiveSupport::Concern
   
   included do
     before_filter :configure_permitted_parameters, if: :devise_controller?
@@ -10,18 +7,6 @@ module DeviseWhitelist
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(account_update, keys: [:name])
-
-   extend ActiveSupport::concern
-   
-   included do
-     before_filter :configure_permitted_perameters, if: :devise_controller?
-   end
-   
-  def configure_permitted_perameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-
-  end
   end
 end
